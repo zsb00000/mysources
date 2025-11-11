0eaa71ca0153da3591d59a9ebe5a8ee26039712c
zsb00000 <2291067504@qq.com>
2025-11-11 14:38:11 +0800

    2025/11/10 14:38

--- 6.cpp
+++ 6.cpp
@@ -1,13 +1,16 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
-constexpr int mod = 9998244353;
+constexpr int mod = 998244353;
 int n, m;
 int s[2005];
 int a[2005][2005];
 int tot = 1;
-int f[2005][4005];
-int main()
+int f[105][4005];
+signed main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
     cin >> n >> m;
     for (int i = 1; i <= n; i++)
     {
@@ -30,6 +33,9 @@ int main()
                 f[i][d + n] = (f[i][d + n] + f[i - 1][d + n] + f[i - 1][d - 1 + n] * a[i][k] % mod + f[i - 1][d + 1 + n] * (s[i] - a[i][k]) % mod) % mod;
             }
         }
+        for (int j = 1; j <= n; j++)
+            tot = (tot - f[n][j + n] + mod) % mod;
     }
+    cout << tot << '\n';
     return 0;
 }
